9a1f9d00231e9e3e027367df96dd9965342a7541
Tito Araujo <titoraujo23@gmail.com>
2022-03-14 11:34:22 -0300

    Ending middleware challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
 const { v4: uuidv4, validate } = require('uuid');
+
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,83 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const userData = users.find(user => user.username === username)
+
+  if(!userData){
+    return response.status(404).json({
+      error: "Username already exists"
+    })
+  }
+
+  request.user =  userData
+
+  return next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+  
+  const verifyProPlan = user.pro
+  const registeredTodos = user.todos.length
+
+  if(verifyProPlan === false && registeredTodos < 10 || verifyProPlan === true ){
+    return next()
+  }else{
+    return response.status(403).json({
+      error: "User not permission for this operation"
+    })
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const validateId = validate(id, 4);
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: "Username not found",
+    });
+  }
+
+  if(!validateId){
+    return response.status(400).json({
+      error: "The id isn't a uuid",
+    });
+  }
+    todoIsFromUser = user.todos.find((todo) => todo.id === id);
+
+    if (!todoIsFromUser) {
+      return response.status(404).json({
+        error: "Todo not belong to user ",
+      });
+    } else {
+      request.todo = todoIsFromUser;
+      request.user = user
+      return next();
+    }
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+ const {id} = request.params
+
+ const userById = users.find(user => user.id === id)
+
+ if(!userById){
+   return response.status(404).json({
+     error:"User not found"
+   })
+ }
+
+ request.user = userById
+
+ return next()
 }
 
 app.post('/users', (request, response) => {
